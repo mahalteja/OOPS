da79874f63cc47eeb9911500eb55d91b7e4897bf
mahalteja <106660291+mahalteja@users.noreply.github.com>
2022-11-08 15:41:53 +0530

    Create oops_basic_syntax.py

---
+++ oops_basic_syntax.py
@@ -0,0 +1,11 @@
+class student:
+    #This is construstor no need to call this method
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    #This is method we have to call this when we required
+    def display(s):
+        print(s.name,s.age)
+s1=student('Mahal',20)
+print(s1.age)
+ 
